c78ceeefa67b75561146effdc028b82441c07c4f
hieubk59 <hieunn.bkict@yahoo.com>
2019-09-04 14:18:06 +0700

    Updates

---
+++ precache-manifest.2765beb818975fdfb4cb45444c84827f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e59715b9044cbdb639e9e410c67ceee",
+    "url": "/github.io/breece-design/index.html"
+  },
+  {
+    "revision": "619fce0cf1a82134ff83",
+    "url": "/github.io/breece-design/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "a14b68fc431ce61f4f48",
+    "url": "/github.io/breece-design/static/js/2.8f33f58a.chunk.js"
+  },
+  {
+    "revision": "619fce0cf1a82134ff83",
+    "url": "/github.io/breece-design/static/js/main.7d131f62.chunk.js"
+  },
+  {
+    "revision": "9a9e1d3a0434be0404f6",
+    "url": "/github.io/breece-design/static/js/runtime~main.c25388c3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github.io/breece-design/static/media/logo.5d5d9eef.svg"
+  }
+]);
